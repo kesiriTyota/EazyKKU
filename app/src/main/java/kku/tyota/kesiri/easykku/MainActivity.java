0d199b067fa4b2edffdc6ae0f182cc8615cb59c2
kesiri <ellouked@gmail.com>
2016-11-12 12:04:32 +0700

    Sign up

--- app/src/main/java/kku/tyota/kesiri/easykku/MainActivity.java
+++ app/src/main/java/kku/tyota/kesiri/easykku/MainActivity.java
@@ -1,13 +1,34 @@
 package kku.tyota.kesiri.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signInButton, signUpButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //sign Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+              //  startActivities(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+    }//main Method
+
+}  //main class
